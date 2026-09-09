c20dce1e42f991b06fa15af067e25399abc6fc91
gabriel mariano <gabrielmarianodeoliveira0@gmail.com>
2026-09-09 16:45:16 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import pandas as pd
 
 
 ##### TÍTULO DA APLICAÇÃO #####
-
-
+st.title("Lobinho Entregas")
 
 ##### Lista de Opções #####
 
@@ -16,10 +15,57 @@ opcoes = ["Buscar CEP", "Descobrir CEP"]
 
 
 ##### BARRA LATERAL #####
-
+st.sidebar.title("lobinho te pega", width=400)
+st.sidebar.image("logo2.png")
+st.sidebar.write("coloque o seu CEP e iremos descobrir o local")
+escolha = st.sidebar.selectbox("escolha uma opção:", opcoes)
 
 ##### BOTÃO BUSCAR CEP #####
 
+if escolha == "Buscar CEP":
+    st.header("Buscar Endereço pelo CEP")
+    st.image("principal.png")
+    cep = st.text_input("Digite o CEP (somente números):")
 
 
 ##### BOTÃO DESCOBRIR CEP #####
+if st.button("Buscar"):
+            if len(cep) != 8 or not cep.isdigit():
+                st.error("Por favor, insira um CEP válido com 8 dígitos numéricos.")
+            else:
+                try:
+                    endereco = BuscarCep.buscar_cep(cep)
+                    if endereco:
+                        st.success("Endereço encontrado:")
+                        st.write(f"CEP: {endereco[0]}")
+                        st.write(f"Endereço: {endereco[1]}")
+                        st.write(f"Bairro: {endereco[2]}")
+                        st.write(f"Cidade: {endereco[3]}")
+                        st.write(f"Estado: {endereco[4]}")
+
+
+                        ## Mapas
+                        st.title("Localização no Mapa")
+                        df = pd.DataFrame({"latitude": [endereco[5]], "longitude": [endereco[6]]})
+                        st.map(df, zoom=15)
+                    else:
+                        st.error("CEP não encontrado.")
+                except Exception as e:
+                    st.error(f"Ocorreu um erro ao buscar o CEP: {e}")
+
+elif escolha == "Descobrir CEP":
+    st.header("Descobrir CEP pelo Endereço")
+    st.image("Descobrir.png")
+    endereco_usuario = st.text_input("Digite o endereço (ex: Rua Olga, Barueri, SP):")
+
+    if st.button("Descobrir"):
+        if not endereco_usuario.strip():
+            st.error("Por favor, insira um endereço válido.")
+        else:
+            try:
+                resultado = BuscarCep.descobrir_cep(endereco_usuario)
+                st.success("Link de busca no Google:")
+                st.write(resultado)
+            except Exception as e:
+                st.error(f"Ocorreu um erro ao descobrir o CEP: {e}")
+
